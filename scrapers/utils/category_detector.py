6627c3b5efefb583631f03493afefa5fb1066861
유향 <kyh6412057153@gmail.com>
2025-12-17 15:01:04 +0900

    fix: Add missing category_detector.py module
    15 scrapers were failing due to missing utils/category_detector module.
    Re-exports detect_category from scraper_utils.py

---
+++ scrapers/utils/category_detector.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Category Detector Module
+Wrapper for detect_category function from scraper_utils
+"""
+
+from utils.scraper_utils import detect_category
+
+__all__ = ['detect_category']
